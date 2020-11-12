e39996a7a3c93e7def5b4455c2cf5e3677a76a34
@ibuninngu <31212444+ibuninngu@users.noreply.github.com>
2020-11-13 00:59:46 +0900

    fix setup

--- setup.py
+++ setup.py
@@ -3,20 +3,23 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+
 setuptools.setup(
     name="Nova-EveFamilia",
     version="0.0.1",
-    install_requires=[
-        "asyncio", "ssl", "glob", "hashlib", "base64"
-    ],
     author="Eve.Familia, Inc.",
     author_email="eve@eve.ninja",
     description="WebApplication Framework",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Eve-Familia-Inc/Nova",
-    packages=setuptools.find_packages("src"),
-    package_dir={"": "src"},
+    project_urls={
+        'Documentation': 'https://nova.eve.ninja/',
+        'Homepage': 'https://nova.eve.ninja/',
+        'Source': 'https://github.com/Eve-Familia-Inc/Nova',
+        'Eve.Familia, Inc.': 'https://www.eve.familia.inc/',
+    },
+    packages=setuptools.find_packages("Nova"),
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
