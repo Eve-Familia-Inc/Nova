00f145ae9348605dd21804909b55acc5176f0270
LobeliaComputing <31212444+ibuninngu@users.noreply.github.com>
2023-04-17 07:12:27 +0900

    _

--- nova/__init__.py
+++ nova/__init__.py
@@ -19,15 +19,15 @@ class Service(server):
     def SetFilesOnMemory(self, path):
         path = (path + "/**").replace("//", "/")
         l = glob.glob(path, recursive=True)
-        root_dir = l[0]
-        print("Root Directory:", root_dir)
-        tmp = [a.replace(root_dir, '/') for a in l]
+        self.root_dir = l[0]
+        print("Root Directory:", self.root_dir)
+        tmp = [a.replace(self.root_dir, '/') for a in l]
         for p in tmp:
             if("." in p):
                 p = p.replace("\\", "/")
                 extension = p.split(".")[-1]
                 if(extension in self.MIME):
-                    f = open(root_dir + p[1:], "rb")
+                    f = open(self.root_dir + p[1:], "rb")
                     data = f.read()
                     f.close()
                     self.OnMemoryFiles[p] = {
@@ -35,6 +35,24 @@ class Service(server):
                         "DATA": data
                     }
                     print("Stored File:", p)
+
+    def Reflesh(self, path):
+        if(path.decode("utf-8") in self.OnMemoryFiles):
+            p = path.decode("utf-8").replace("\\", "/")
+            extension = p.split(".")[-1]
+            if(extension in self.MIME):
+                f = open(self.root_dir + p[1:], "rb")
+                data = f.read()
+                f.close()
+                self.OnMemoryFiles[p] = {
+                    "MIME": self.MIME[extension],
+                    "DATA": data
+                }
+                print("Stored File:", p)
+
+    async def Get(self, connection, Request, ReplyHeader):
+        self.Reflesh(Request["path"])
+        await super().Get(connection, Request, ReplyHeader)
 
     def EnableSSL(self, domain_cert, private_key):
         ctx = ssl.SSLContext(ssl.PROTOCOL_TLS)
